a73a98f68c468dd7a733bb450375ab36e7920b26
Ramiz <ramiz.jaffarove@gmail.com>
2020-05-13 21:16:33 -0700

    intermediate

---
+++ WorkflowService/Workflow.Tests/Services/ScopesServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Sqlite;
+using NUnit.Framework;
+
+namespace Workflow.Tests.Services
+{
+    [TestFixture]
+    public class ScopesServiceTests
+    {
+        private SqliteConnection _dbConnection;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dbConnection = ContextHelper.OpenSqliteInMemoryConnection();
+            using var dataContext = ContextHelper.CreateContext(_dbConnection, true);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
