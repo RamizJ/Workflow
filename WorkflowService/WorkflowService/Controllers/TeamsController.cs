6a219fe1e169ad178e5b861120c085ae9ca5cc53
RamizJ <ramiz.jaffarove@gmail.com>
2020-06-02 11:54:55 -0700

    Merge TeamUsersController into TeamsController. Add CurrentUser method into UsersController

--- WorkflowService/WorkflowService/Controllers/TeamsController.cs
+++ WorkflowService/WorkflowService/Controllers/TeamsController.cs
@@ -20,10 +20,13 @@ namespace WorkflowService.Controllers
         /// </summary>
         /// <param name="userManager"></param>
         /// <param name="teamsService"></param>
-        public TeamsController(UserManager<ApplicationUser> userManager, ITeamsService teamsService)
+        /// <param name="teamUsersService"></param>
+        public TeamsController(UserManager<ApplicationUser> userManager, 
+            ITeamsService teamsService, ITeamUsersService teamUsersService)
         {
             _userManager = userManager;
             _teamsService = teamsService;
+            _teamUsersService = teamUsersService;
         }
 
         /// <summary>
@@ -58,6 +61,29 @@ namespace WorkflowService.Controllers
                 filter, filterFields, sortFields, withRemoved);
         }
 
+
+        /// <summary>
+        /// Постраничная загрузка пользователей команды с фильтрацией и сортировкой
+        /// </summary>
+        /// <param name="teamId">Идентификатор команды</param>
+        /// <param name="pageNumber">Номер страницы</param>
+        /// <param name="pageSize">Размер страницы</param>
+        /// <param name="filter">Фильтр по всем полям</param>
+        /// <param name="filterFields">Конкретные поля фильтрации</param>
+        /// <param name="sortFields">Поля сортировки</param>
+        /// <param name="withRemoved">Вместе с удаленными</param>
+        /// <returns>Коллекция пользователей команды</returns>
+        [HttpGet]
+        public async Task<IEnumerable<VmUser>> GetUsersPage([FromQuery] int teamId,
+            [FromQuery] int pageNumber, [FromQuery] int pageSize,
+            [FromQuery] string filter, [FromQuery] FieldFilter[] filterFields,
+            [FromQuery] FieldSort[] sortFields, [FromQuery] bool withRemoved = false)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            return await _teamUsersService.GetPage(user, teamId, pageNumber, pageSize, 
+                filter, filterFields, sortFields, withRemoved);
+        }
+
         /// <summary>
         /// Получить команд по идентификаторам
         /// </summary>
@@ -81,6 +107,20 @@ namespace WorkflowService.Controllers
         {
             var currentUser = await _userManager.GetUserAsync(User);
             return await _teamsService.Create(currentUser, team);
+        }
+
+        /// <summary>
+        /// Добавление пользователя в команду
+        /// </summary>
+        /// <param name="teamId"></param>
+        /// <param name="userId">Идентификатор добавляемого пользователя</param>
+        /// <returns>Добавленный пользователь</returns>
+        [HttpPost("{teamId}")]
+        public async Task<IActionResult> AddUser(int teamId, [FromBody] string userId)
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            await _teamUsersService.Add(currentUser, teamId, userId);
+            return NoContent();
         }
 
         /// <summary>
@@ -115,8 +155,23 @@ namespace WorkflowService.Controllers
             return Ok(deletedScope);
         }
 
+        /// <summary>
+        /// Удаление пользователя из команды
+        /// </summary>
+        /// <param name="teamId">Идентификатор команды</param>
+        /// <param name="userId">Идентификатор пользователя</param>
+        /// <returns>Удаленный пользователь</returns>
+        [HttpDelete("{teamId}/{userId}")]
+        public async Task<IActionResult> RemoveUser(int teamId, string userId)
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            await _teamUsersService.Remove(currentUser, teamId, userId);
+            return NoContent();
+        }
+
 
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ITeamsService _teamsService;
+        private readonly ITeamUsersService _teamUsersService;
     }
 }
