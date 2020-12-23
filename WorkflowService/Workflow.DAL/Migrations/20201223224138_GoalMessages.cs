54ddaa5d82caf8d5a5b80b51f3fdbededd6120a1
RamizJ <ramiz.jaffarove@gmail.com>
2020-12-23 14:46:00 -0800

    Goal message migrations

---
+++ WorkflowService/Workflow.DAL/Migrations/20201223224138_GoalMessages.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Workflow.DAL.Migrations
+{
+    public partial class GoalMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Goals",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "GoalMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GoalId = table.Column<int>(type: "int", nullable: false),
+                    OwnerId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    LastEditDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsRemoved = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GoalMessages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GoalMessages_AspNetUsers_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_GoalMessages_Goals_GoalId",
+                        column: x => x.GoalId,
+                        principalTable: "Goals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGoalMessages",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    GoalMessageId = table.Column<int>(type: "int", nullable: false),
+                    LastReadingDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGoalMessages", x => new { x.GoalMessageId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_UserGoalMessages_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserGoalMessages_GoalMessages_GoalMessageId",
+                        column: x => x.GoalMessageId,
+                        principalTable: "GoalMessages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GoalMessages_GoalId",
+                table: "GoalMessages",
+                column: "GoalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GoalMessages_OwnerId",
+                table: "GoalMessages",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGoalMessages_UserId",
+                table: "UserGoalMessages",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserGoalMessages");
+
+            migrationBuilder.DropTable(
+                name: "GoalMessages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Goals",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(512)",
+                oldMaxLength: 512);
+        }
+    }
+}
