08ed22b31da7ee1803f0535607ac277bae59e1d9
RamizJ <ramiz.jaffarove@gmail.com>
2020-12-23 16:54:35 -0800

    fixes

--- WorkflowService/WorkflowService/Startup.cs
+++ WorkflowService/WorkflowService/Startup.cs
@@ -190,6 +190,7 @@ namespace WorkflowService
             services.AddTransient<IWorkloadForProjectStatisticService, WorkloadForProjectStatisticService>();
             services.AddTransient<IWorkloadByDaysStatisticService, WorkloadByDaysStatisticService>();
             services.AddTransient<ITotalStatisticService, TotalStatisticService>();
+            services.AddTransient<IGoalMessageService, GoalMessageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
