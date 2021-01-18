b3f80429fbd0b097ccde15b575cb212e65801455
RamizJ <ramiz.jaffarove@gmail.com>
2021-01-17 19:07:33 -0800

    Add estimate and actual performing time for change goal state method

--- WorkflowService/Workflow.VM/ViewModels/VmGoalState.cs
+++ WorkflowService/Workflow.VM/ViewModels/VmGoalState.cs
@@ -1,11 +1,30 @@
-﻿using Workflow.DAL.Models;
+﻿using Workflow.DAL.Migrations;
+using Workflow.DAL.Models;
 
 namespace Workflow.VM.ViewModels
 {
     public class VmGoalState
     {
         public int GoalId { get; set; }
+        
+        /// <summary>
+        /// Статус выполнения задачи
+        /// </summary>
         public GoalState GoalState { get; set; }
+        
+        /// <summary>
+        /// Комментарий
+        /// </summary>
         public string Comment { get; set; }
+
+        /// <summary>
+        /// Примерное время выполнения в часах
+        /// </summary>
+        public double? EstimatedPerformingHours { get; set; }
+        
+        /// <summary>
+        /// Фаактическое время выполнения в часах 
+        /// </summary>
+        public double? ActualPerformingHours { get; set; }
     }
 }
